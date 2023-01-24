8ec20e7ec5ad7729259996e65f98865083602d76
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-01-24 19:49:38 +0600

    react router dom is added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-   <button className="btn">Button</button>
-<button className="btn btn-primary">Button</button>
-<button className="btn btn-secondary">Button</button>
-<button className="btn btn-accent">Button</button>
-<button className="btn btn-ghost">Button</button>
-<button className="btn btn-link">Button</button>
+    <div className="">
+      <RouterProvider >
+
+      </RouterProvider>
+
     </div>
   );
 }
