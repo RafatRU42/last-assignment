eb552d24f7cec55e6e97de7d91a07ab9851f4282
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-02-03 11:51:15 +0600

    git check

--- src/Shared/Navbar/Navbar.js
+++ src/Shared/Navbar/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
               <li><a>Homepage</a></li>
               <li><a>Portfolio</a></li>
-              <li><a>About</a></li>
+              <li><a>Rafat</a></li>
               
             </ul>
           </div>
