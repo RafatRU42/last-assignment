9dfe8294047607d61c1d823af2402be0b589b923
Ruhul Muazzam Rafat <rmrafat420@gmail.com>
2023-02-06 22:42:01 +0600

    image uploading

--- src/Shared/Routes/AddProducts.js
+++ src/Shared/Routes/AddProducts.js
@@ -1,15 +1,57 @@
 import { useContext } from 'react';
 import { useForm } from 'react-hook-form';
+import { toast } from 'react-hot-toast';
 import { authContext } from '../Context/Authprovider';
 
 const AddProducts = () => {
-    const {user} = useContext(authContext)
+    const { user } = useContext(authContext)
 
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const onSubmit = data => {
         console.log(data)
-    console.log('user',user)
+
+        const userName = user.displayName;
+        const userEmail = user.email;
+        const productName = data.name;
+        const category = data.category;
+        const price = data.price;
+        const location = data.location;
+        const condition = data.condition;
+        const number = data.mobileNumber;
+
+        const productInformation = {
+            userName,
+            userEmail,
+            productName,
+            category,
+            price,
+            location,
+            condition,
+            number,
+        }
+
+
+
+        fetch('http://localhost:5000/addProduct', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(productInformation)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('data', data)
+                if (data.acknowledged) {
+                    toast.success('You Successfully Added the Product !')
+                }
+                else {
+                    toast.error(data.message)
+                }
+            })
+
+
 
     };
     return (
@@ -23,12 +65,24 @@ const AddProducts = () => {
                     <div className="space-y-4">
                         <div>
                             <label for="text" className="block mb-2 text-sm">Product Name</label>
-                            <input type="text" {...register("name", { required: 'You Have to Insert Product Name' })} placeholder="Please Insert Product Name" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 light:bg-gray-900 light:text-gray-100" />
+                            <select  {...register("name", { required: true })} className="w-full px-3 py-2 border rounded-md dark:border-gray-700 light:bg-gray-900 light:text-gray-100">
+                                <option disabled> Please Select A Product Name</option>
+                                <option>Samsung</option>
+                                <option>VIVO</option>
+                                <option>IPhone</option>
+
+                            </select>
                             {errors.name && <span className='text-red-400'>{errors.name?.message}</span>}
                         </div>
                         <div>
                             <label for="text" className="block mb-2 text-sm">Product Category</label>
-                            <input type="text" {...register("category", { required: true })} placeholder="Please Insert Product Category" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 light:bg-gray-900 light:text-gray-100" />
+                            <select type="text" {...register("category", { required: true })} placeholder="Please Insert Product Category" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 light:bg-gray-900 light:text-gray-100">
+                                <option>Mobile </option>
+                                <option>TV</option>
+                                <option>Electronics</option>
+                                <option>Bike</option>
+                                <option>Other</option>
+                            </select>
                         </div>
                         <div>
                             <label for="text" className="block mb-2 text-sm">Price</label>
@@ -36,7 +90,15 @@ const AddProducts = () => {
                         </div>
                         <div>
                             <label for="text" className="block mb-2 text-sm">Location</label>
-                            <input type="text" {...register("location", { required: true })} placeholder="Please Insert Product Location" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 light:bg-gray-900 light:text-gray-100" />
+                            <select {...register("location", { required: true })} className="w-full px-3 py-2 border rounded-md dark:border-gray-700 light:bg-gray-900 light:text-gray-100">
+                                <option disabled>Please Select A Location</option>
+                                <option>Dhaka</option>
+                                <option>Rajshahi</option>
+                                <option>Chattagram</option>
+                                <option>Khulna</option>
+                                <option>Barishal</option>
+                                <option>Ranjpur</option>
+                            </select>
                         </div>
                         <div>
                             <label for="text" className="block mb-2 text-sm">Product Condition</label>
@@ -45,6 +107,10 @@ const AddProducts = () => {
                         <div>
                             <label for="number" className="block mb-2 text-sm">Mobile Number</label>
                             <input type="number" {...register("mobileNumber", { required: true })} placeholder="Please Insert Mobile Number" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 light:bg-gray-900 light:text-gray-100" />
+                        </div>
+                        <div>
+                            <label for="image" className="block mb-2 text-sm">Product Image</label>
+                            <input type="file" {...register("image", { required: true })} placeholder="Please Insert Mobile Number" className="w-full px-3 py-2 border rounded-md dark:border-gray-700 light:bg-gray-900 light:text-gray-100" />
                         </div>
 
 
